8f115876157c109ce7635241ad043758065a9a4b
Stuart Blair <stuart@5values.com>
2011-12-20 11:24:49 -0800

    Configuring the LightWeightJNDIContextFactory with this cut-down JNDIContext

--- lib/reigns/jndi_context.rb
+++ lib/reigns/jndi_context.rb
@@ -1,11 +1,13 @@
 require 'java'
 java_import "java.lang.System"
+java_import "com._5values.jndistub.LightweightJNDIContextFactory"
 
 module Reigns
   class JNDIContext
     def initialize(mappings)
       System.setProperty("java.naming.factory.initial", "com._5values.jndistub.LightweightJNDIContextFactory")
       @mappings = mappings
+      LightweightJNDIContextFactory.set_context(self)
     end
 
     def lookup_object(name)
